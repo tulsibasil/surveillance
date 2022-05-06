9c044b89d75d27d703933822c5bd8ef76782b7f4
tulsibasil <tulsi.patel@yale.edu>
2022-05-06 14:38:56 -0400

    save ur work

---
+++ surveillance.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//     $('#myaudio').get(0).play();
+// });
+
+// window.onClick = function() {
+//     document.getElementById("myaudio").play();
+// }
+// console.log("play")
+
+// document.addEventListener('click', musicPlay);
+// function musicPlay() {
+//     document.getElementById('myaudio').play();
+// }
